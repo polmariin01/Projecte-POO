835a67c2fd8e24c38f5d8d0a1d420a792c1fec9b
gilboleda <58255939+gilboleda@users.noreply.github.com>
2020-04-15 01:49:06 +0200

    La clase Entrades Blog ja esta quasi acabada

--- Projecte-Poo/src/projectepoo/EntradesBlog.java
+++ Projecte-Poo/src/projectepoo/EntradesBlog.java
@@ -5,11 +5,81 @@
  */
 package projectepoo;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author Usuari
  * Fa: Gil
  */
 public class EntradesBlog {
+    public java.lang.String criteri;
+    public LinkedList<Entrada> Entrades;
     
+    public EntradesBlog(){
+        this.criteri = "num";
+        this.Entrades = new LinkedList<>();
+    }
+    public boolean setCriteri(java.lang.String criteri){
+        if(criteri.equals("num") || criteri.equals("titol")){
+            this.criteri = criteri;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void afageixOrdenat(Entrada e){
+        boolean a =  this.Entrades.add(e);
+        this.ordena();
+    }
+    public Entrada agafa(int num){
+        if((num > Entrades.size() -1)||(num < 0)){
+            return null;
+        }else{
+            return this.Entrades.get(num);
+        }
+    }
+    public Entrada elimina(int num){
+         if((num > Entrades.size() -1)||(num < 0)){
+            return null;
+        }else{
+            return this.Entrades.remove(num);
+        }
+    }
+    public void ordena(){
+        if(this.criteri.equals("num")){
+           this.Entrades.iterator();
+        }
+        
+    }
+    public java.lang.String index(){
+        java.lang.String tornar = "";
+        this.ordena();
+        for(int i = 0; i<this.Entrades.size();i++){
+            tornar = tornar + this.Entrades.get(i).getTitol() + " " + this.Entrades.get(i).getId() + "\n";
+        }
+        return tornar ;
+    }
+    public java.lang.String entreDates(Temps inici,Temps fi){
+        java.lang.String tornar = "";
+        this.ordena();
+        for(int i = 0; i<this.Entrades.size();i++){
+            if(this.Entrades.get(i).temps.comparaTemps(inici) >= 0){
+                if (this.Entrades.get(i).temps.comparaTemps(fi) > 0) {
+                } else {
+                    tornar = tornar + this.Entrades.get(i).text + "\n";
+                }
+            }
+        }
+        return tornar ;
+    }
+    @Override
+    public java.lang.String toString(){
+        this.ordena();
+        java.lang.String tornar = "";
+        for(int i = 0; i<this.Entrades.size();i++){
+            tornar = tornar + this.Entrades.get(i).text + "\n";
+        }
+        return tornar;
+    }
 }
--- Projecte-Poo/src/projectepoo/Mainproves.java
+++ Projecte-Poo/src/projectepoo/Mainproves.java
@@ -13,23 +13,33 @@ public class Mainproves {
    
     
     public static void main (String[] args){
-        Entrada e1 = new Entrada("Entrada 1","Aquest es el text de la entrada 1","");
-        Entrada e2 = new Entrada("Titol entrada 2","Aquest es el text de la entrada 2","2020-02-12 12:02:12");
- 
-        System.out.println("Id: "+e1.getId());
+        Entrada e1 = new Entrada("Entrada 1","Aquest es el text de la entrada 1","2020-01-01 00:00:00");
+        Entrada e2 = new Entrada("Titol entrada 2","Aquest es el text de la entrada 2","2022-02-12 12:02:12");
+        Entrada e3 = new Entrada("Titol entrada 3","Aquest es el text de la entrada 3","2024-02-12 12:02:12");
+        EntradesBlog Entrades = new EntradesBlog();
+        Entrades.afageixOrdenat(e2);
+        Entrades.afageixOrdenat(e3);
+        Entrades.afageixOrdenat(e1);
+        System.out.println(Entrades.index());
+       
+        System.out.println(Entrades.toString());
+        //java.lang.String a = Entrades.entreDates(inici,fi);
+        
+        
+        
+        
+        /*System.out.println("Id: "+e1.getId());
         System.out.println("Titol: "+e1.getTitol());
         System.out.println("Temps: "+e1.getQuan());
         System.out.println();
-
         System.out.println("Id: "+e2.getId());
         System.out.println("Titol: "+e2.getTitol());
         System.out.println("Temps: "+e2.getQuan());
-        
         int a = Entrada.compare(e1,e2,"num");
         int b = Entrada.compare(e1,e2,"titol");
         int c = Entrada.compare(e1,e2,"vaina");
+        System.out.println(a+"  "+b+"  "+c);*/
         
-        System.out.println(a+"  "+b+"  "+c);
     }
     
 }
