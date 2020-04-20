683b429504cf4b4b3b0f56be596d6a794b3305e5
polmariin01 <45402851+polmariin01@users.noreply.github.com>
2020-04-20 22:01:19 +0200

    Control data entrada format correcte

--- Projecte-Poo/src/projectepoo/Controlador.java
+++ Projecte-Poo/src/projectepoo/Controlador.java
@@ -50,8 +50,18 @@ public class Controlador {
         String t = IO.llegeixText();
         IO.mostrarText("Text? ");       
         String ttx = IO.llegeixText();
-        IO.mostrarText("Dia i hora? ");        
-        String d = IO.llegeixText();        //Falta posar 'ARA' i control d'errors en el format de la data
+        String d;
+        do{
+            IO.mostrarText("Dia i hora? ");
+            d = IO.llegeixText();        //Falta posar 'ARA' i control d'errors en el format de la data
+            if (!this.comprobarFormatData(d)){
+                if (d.equalsIgnoreCase("ara")){
+                    d=" ";
+                } else {
+                    IO.mostrarText("\nFormat de la data incorrecte.\n");
+                }
+            }
+        } while (!this.comprobarFormatData(d) && !d.equals(" "));
         
         e = new Entrada(t,ttx,d);
         this.entradesBlog.afageixOrdenat(e);
@@ -59,5 +69,29 @@ public class Controlador {
     
     public void ordena(){
         this.entradesBlog.ordena();
+    }
+
+    private boolean comprobarFormatData(java.lang.String s){
+        java.lang.String[] data = s.split(" ");
+        if (data.length < 2){
+            return false;
+        }
+        java.lang.String[] day = data[0].split("-"), hour = data[1].split(":");
+        if (day.length < 3 || hour. length<3){
+            return false;
+        }
+        
+        int any = Integer.valueOf(day[0]), mes = Integer.valueOf(day[1]), dia = Integer.valueOf(day[2]);
+        int hora = Integer.valueOf(hour[0]), min = Integer.valueOf(hour[1]), seg = Integer.valueOf(hour[2]);
+        
+        if (any<0 || mes<0 || dia<0 || hora<0 || min<0 || seg<0 ){
+            return false;
+        }
+        
+        if (any>2020 || mes>12 || dia>31 || hora>24 || min>60 || seg>60 ){
+            return false;
+        }
+        
+        return true;
     }
 }
