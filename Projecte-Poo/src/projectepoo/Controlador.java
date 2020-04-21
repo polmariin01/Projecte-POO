41f6068c2ef47edc525055d838108673ace24927
polmariin01 <45402851+polmariin01@users.noreply.github.com>
2020-04-21 18:54:52 +0200

    Update Controlador.java

--- Projecte-Poo/src/projectepoo/Controlador.java
+++ Projecte-Poo/src/projectepoo/Controlador.java
@@ -20,6 +20,38 @@ public class Controlador {
 
     public void dates() {
         //estic fent, no tocar
+
+        java.lang.String data1, data2, anyi, anyf, mesi, mesf, diai, diaf;
+        java.lang.String[] datai, dataf;
+        /*
+        IO.mostrarText("Dia inicial? (aaaa-mm-dd) ");
+        data1 = IO.llegeixText();
+        datai = data1.split("-");
+        anyi= datai[0];
+        mesi = datai[2];
+        diai = datai[3];
+        IO.mostrarText("Dia final? (aaaa-mm-dd) ");
+        data2 = IO.llegeixText();
+        dataf = data2.split("");
+        anyi= dataf[0];
+        mesi = dataf[2];
+        diai = dataf[3];
+        
+        for (int i=0; i<this.entradesBlog.Entrades.size(); i++){
+            if()
+        }
+         */
+        IO.mostrarText("Dia inicial? (aaaa-mm-dd) ");
+        data1 = IO.llegeixText();
+        IO.mostrarText("Dia final? (aaaa-mm-dd) ");
+        data2 = IO.llegeixText();
+
+        for (int i = 0; i < this.entradesBlog.Entrades.size(); i++) {
+            if (data1.compareTo(this.entradesBlog.Entrades.get(i).temps.toString())<0 &&
+                    + (data2.compareTo(this.entradesBlog.Entrades.get(i).temps.toString()))> 0) {
+                IO.mostrarText(this.entradesBlog.Entrades.get(i).toString() + "\n");
+            }
+        }
     }
 
     public void eliminaEntrada(int num) {
@@ -78,6 +110,8 @@ public class Controlador {
             Temps temps = new Temps();
             temps.ara();
             d = temps.dia + " " + a;
+        } else if(a == 3){
+            d = d + " 00:00:00";
         }
 
         e = new Entrada(t, ttx, d);
@@ -99,35 +133,45 @@ public class Controlador {
     }
 
     private int comprobarFormatData(java.lang.String s) {
-        java.lang.String[] hour;
+        java.lang.String[] time;
         if (s.isBlank()) {
             return 0;
         }
         java.lang.String[] data = s.split(" ");
         if (data.length < 2) {
             if (data.length == 1) {
-                hour = data[0].split(":");
-                if (hour.length != 3) {
-                    return -1;
-                } else {
-                    if (Integer.valueOf(hour[0]) < 0 || Integer.valueOf(hour[1]) < 0 || Integer.valueOf(hour[2]) < 0) {
+                time = data[0].split(":");
+                if (time.length == 3) {
+                    if (Integer.valueOf(time[0]) < 0 || Integer.valueOf(time[1]) < 0 || Integer.valueOf(time[2]) < 0) {
                         return -1;
-                    } else if (Integer.valueOf(hour[0]) >= 24 || Integer.valueOf(hour[1]) >= 60 || Integer.valueOf(hour[2]) >= 60) {
+                    } else if (Integer.valueOf(time[0]) >= 24 || Integer.valueOf(time[1]) >= 60 || Integer.valueOf(time[2]) >= 60) {
                         return -1;
                     } else {
                         return 1;
                     }
+                } else {
+                    time = data[0].split("-");
+                    if (time.length == 3) {
+                        if (Integer.valueOf(time[0]) < 0 || Integer.valueOf(time[1]) < 0 || Integer.valueOf(time[2]) < 0) {
+                            return -1;
+                        } else if (Integer.valueOf(time[1]) > 12 || Integer.valueOf(time[2]) > 31) {
+                            return -1;
+                        } else {
+                            return 3;
+                        }
+                    }
+                return -1;
                 }
             }
         }
         java.lang.String[] day = data[0].split("-");
-        hour = data[1].split(":");
-        if (day.length < 3 || hour.length < 3) {
+        time = data[1].split(":");
+        if (day.length < 3 || time.length < 3) {
             return -1;
         }
 
         int any = Integer.valueOf(day[0]), mes = Integer.valueOf(day[1]), dia = Integer.valueOf(day[2]);
-        int hora = Integer.valueOf(hour[0]), min = Integer.valueOf(hour[1]), seg = Integer.valueOf(hour[2]);
+        int hora = Integer.valueOf(time[0]), min = Integer.valueOf(time[1]), seg = Integer.valueOf(time[2]);
 
         if (any < 0 || mes < 0 || dia < 0 || hora < 0 || min < 0 || seg < 0) {
             return -1;
