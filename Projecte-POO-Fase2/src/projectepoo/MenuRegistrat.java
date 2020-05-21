79670e28ba7956e131d1c24123877202d56b1315
gilboleda <58255939+gilboleda@users.noreply.github.com>
2020-05-21 19:37:33 +0200

    ..

--- Projecte-POO-Fase2/src/projectepoo/MenuRegistrat.java
+++ Projecte-POO-Fase2/src/projectepoo/MenuRegistrat.java
@@ -10,5 +10,59 @@ package projectepoo;
  * @author Usuari
  */
 public class MenuRegistrat extends Menu {
+
+    public MenuRegistrat(Controlador contr) {
+        super(contr);
+    }
+    @Override
+    public void executaOpcio(String op) {
+        java.lang.String com[] = op.split(" ");
+        com[0] = com[0].toUpperCase();
+
+        if("INDEX".startsWith(com[0])){
+            controlador.index();
+        } else if("MOSTRAR".startsWith(com[0])){
+            if (com.length == 2 && Integer.valueOf(com[1]) >= 0) {
+                IO.mostrarText("\n");
+                controlador.mostraEntrada(Integer.valueOf(com[1]));
+                IO.mostrarText("\n");
+            } else {
+                IO.mostrarText("Entrada incorrecte.\n");
+            }
+            
+        } else if("NOVA".startsWith(com[0])){
+            controlador.novaEntrada();
+
+        } else if("ELIMINA".startsWith(com[0])){
+            if (com.length == 2 && Integer.valueOf(com[1]) >= 0) {
+                controlador.eliminaEntrada(Integer.valueOf(com[1]));
+            } else {
+                IO.mostrarText("Entrada incorrecte.\n");
+            }
+            
+        } else if("ORDENA".startsWith(com[0])){
+            controlador.ordena();
+            
+        } else if("DATES".startsWith(com[0])){
+            controlador.dates();
+            
+        } else if("TOTES".startsWith(com[0])){
+            controlador.mostraEntrades();
+            
+        } else if("FI".startsWith(com[0])){
+            IO.mostrarText("Adiós!");
+            System.exit(0);
+            
+        } else {
+            IO.mostrarText("El text introduit no coincideix amb cap opcio.\n");
+        }
+    }
+
+    @Override
+    public void mostraOpcions() {
+        IO.mostrarText("\nOpcions: \n\n\tIndex\n\tMostra <num>\n\tNova\n\t"
+                + "Elimina <num>\n\tOrdena\n\tDates\n\t"
+                + "Totes\n\tFi\n\nIntrodueix una opció: ");
+    }
     
 }
